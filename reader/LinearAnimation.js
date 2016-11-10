63d4326f6c8bbf765a5e0b944fb04799ee86c65f
João Castro <up201404896@fe.up.pt>
2016-11-10 09:31:18 +0000

    na verdade não fizeste nada

--- reader/LinearAnimation.js
+++ reader/LinearAnimation.js
@@ -2,20 +2,52 @@ var LinearAnimation = function(pontoscontrolo, tempo, scene) {
     Animation.apply(this, arguments);
     
     this.size = 0;
-    this.tempo = tempo;   
+    this.tempo = tempo;
+    this.pontoscontrolo = pontoscontrolo;
+    
+    
 
 };
 
 LinearAnimation.prototype = Object.create(Animation.prototype);
 LinearAnimation.prototype.constructor = LinearAnimation;
 
-LinearAnimation.prototype.speed = function(pontos) {
+LinearAnimation.prototype.calculateVectors = function () {
 	
-	var speed = [];
+	vetores = [];
+ 
+	for (var i = 1; i < this.pontoscontrolo.length; i++)
+		{
+			vetor = [];
+		
+			vetor[0] = pontoscontrolo[i][0] - pontoscontrolo[i-1][0];
+			vetor[1] = pontoscontrolo[i][1] - pontoscontrolo[i-1][1];
+			vetor[2] = pontoscontrolo[i][2] - pontoscontrolo[i-1][2];
+			
+			vetores.push(vetor);
+		}
 	
-	speed[0] = pontos[0]/tempo;
-	speed[1] = pontos[1]/tempo;
-	speed[2] = pontos[2]/tempo;
+	increment = [];
+	
+	for (var i = 0; i<vetores.length; i++){
+		
+		var time = this.tempo/vetores.length-1;
+		
+		increment.push(this.incrementacao(vetores[i], time));
+	}
+		
+}
+
+
+LinearAnimation.prototype.incrementacao = function(pontos, time) {
+	
+	var inc = [];
+	
+	inc[0] = pontos[0]/time;
+	inc[1] = pontos[1]/time;
+	inc[2] = pontos[2]/time;
+	
+	return inc;
  
 }
 
