4c392696fb4e505387251822cbbd473cc30b0aa4
José Oliveira <jose.j.p.oliveira.96@gmail.com>
2016-11-17 21:07:24 +0000

    FIRST VERSION OF VEHICLE IMPLEMENTED

---
+++ reader/primitives/MyVehicle.js
@@ -0,0 +1,208 @@
+function MyVehicle(scene) {
+  this.scene = scene;
+
+  /*this.front = new MyPatch(scene,3,3,20,20,[	// U = 0
+      [ // V = 0..3;
+        [ -0, -0, 1.5, 1 ],
+        [ -0, 0, 1.5, 1 ],
+        [ -0, 0, 1.5, 1 ],
+        [ -0, 0, 1.5, 1 ]
+
+      ],
+      // U = 1
+      [ // V = 0..3
+        [ -0, -0, 1.5, 1 ],
+        [ -20, -5, 8, 1 ],
+        [ -20, 5, 8, 1 ],
+        [ -0, 0, 1.5, 1 ]
+      ],
+      // U = 2
+      [ // V = 0..3
+        [ 0, -0, 1.5, 1 ],
+        [ 20, -5, 8, 1 ],
+        [ 20, 5, 8, 1 ],
+        [ 0, 0, 1.5, 1 ]
+      ],
+      // U = 3
+      [ // V = 0..3
+        [ 0, -0, 1.5, 1 ],
+        [ 0, 0, 1.5, 1 ],
+        [ 0, 0, 1.5, 1 ],
+        [ 0, 0, 1.5, 1 ]
+      ]
+    ])*/
+
+
+    this.front = new MyPatch(scene,3,3,20,20,[	// U = 0
+        [ // V = 0..3;
+          [ -2, -.5, 0, 1 ],
+          [ -10, -.3, 0, 1 ],
+          [ -10, .3, 0, 1 ],
+          [ -2, .5, 0, 1 ]
+
+        ],
+        // U = 1
+        [ // V = 0..3
+          [ -2, -.5, 0, 1 ],
+          [ -5, -.5, 3, 1 ],
+          [ -5, .5, 3, 1 ],
+          [ -2, .5, 0, 1 ]
+        ],
+        // U = 2
+        [ // V = 0..3
+          [ -2, -1, 0, 1 ],
+          [ 10, -.5, 5, 1 ],
+          [ 10, .5, 5, 1 ],
+          [ -2, 1, 0, 1 ]
+        ],
+        // U = 3
+        [ // V = 0..3
+          [ -2, -.5, 0, 1 ],
+          [ 5, -.3, 0, 1 ],
+          [ 5, .3, 0, 1 ],
+          [ -2, .5, 0, 1 ]
+        ]
+      ])
+
+      this.back = new MyPatch(scene,2,3,20,20,[	// U = 0
+          [ // V = 0..3;
+            [ -2, -.5, 0, 1 ],
+            [ -10, -.3, 0, 1 ],
+            [ -10, .3, 0, 1 ],
+            [ -2, .5, 0, 1 ]
+
+          ],
+          // U = 1
+          [ // V = 0..3
+            [ -2, -.5, 0, 1 ],
+            [ -5, -.5, 3, 1 ],
+            [ -5, .5, 3, 1 ],
+            [ -2, .5, 0, 1 ]
+          ],
+          // U = 2
+          [ // V = 0..3
+            [ -2, -.5, 0, 1 ],
+            [ 5, -.3, 0, 1 ],
+            [ 5, .3, 0, 1 ],
+            [ -2, .5, 0, 1 ]
+          ]
+        ])
+
+  this.topWing = new MyPatch(scene,2,3,20,20,[	// U = 0
+      [ // V = 0..3;
+        [ -1, -10, 0, 1 ],
+        [ -1, -0, 0, 1 ],
+        [ -1, 0, 0, 1 ],
+        [ -1, 10, 0, 1 ]
+
+      ],
+      // U = 1
+      [ // V = 0..3
+        [ -0.1, -3, 0.3, 1 ],
+        [ -0.1, 0, 0.5, 1 ],
+        [ -0.1, 0, 0.5, 1 ],
+        [ -0.1, 3, 0.3, 1 ]
+      ],
+      // U = 2
+      [ // V = 0..3
+        [ -1, -10, 0, 1 ],
+        [ 1, 0, 0, 1 ],
+        [ 1, 0, 0, 1 ],
+        [ -1, 10, 0, 1 ]
+      ]
+    ])
+
+  this.bottomWing = new MyPatch(scene,2,3,20,20,[	// U = 0
+    [ // V = 0..3;
+      [ 1, -10, 0.0, 1 ],
+      [ -1, -2.0, 0.0, 1 ],
+      [ -1,  2.0, 0.0, 1 ],
+      [ 1,  10, 0.0, 1 ]
+
+    ],
+    // U = 1
+    [ // V = 0..3
+       [ 0, -0, 0.0, 1 ],
+       [ -0, -0, 0.0, 1],
+       [ -0,  0, 0.0, 1 ],
+       [ 0,  0, 0.0, 1 ]
+    ],
+    // U = 2
+    [ // V = 0..3
+      [ 1, -10, 0.0, 1 ],
+      [ 1, -0.0, 0.0, 1 ],
+      [ 1,  0.0, 0.0, 1 ],
+      [ 1,  10, 0.0, 1 ]
+    ]
+  ]);
+
+  this.bottom = new MyPlane(scene,14,2,20,20);
+  this.fuselage = new MyPatch(scene,2,1,20,20,	[	// U = 0
+      [ // V = 0..1;
+         [ -1, -7, 0.0, 1 ],
+         [ -1,  7, 0.0, 1 ]
+
+      ],
+      // U = 1
+      [ // V = 0..1
+         [ 0, -8.5, 3.0, 1 ],
+         [ 0,  1.5, 3.0, 1 ]
+      ],
+      // U = 2
+      [ // V = 0..1
+        [ 1, -7, 0.0, 1 ],
+        [ 1,  7, 0.0, 1 ]
+      ]
+    ])
+  };
+
+MyVehicle.prototype = Object.create(CGFnurbsObject.prototype);
+MyVehicle.prototype.constructor = MyVehicle;
+
+MyVehicle.prototype.display = function() {
+/*
+  this.scene.pushMatrix();
+    this.scene.rotate(Math.PI/2, 0 ,0, 1);
+    this.fuselage.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+		this.scene.rotate(Math.PI, 0 ,1, 0);
+    this.bottom.display();
+  this.scene.popMatrix();
+*/
+  this.scene.pushMatrix();
+    this.scene.translate(-2,0,0);
+		this.scene.rotate(Math.PI, 0 ,0, 1);
+    this.topWing.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+    this.scene.translate(-2,0,0);
+		this.scene.rotate(Math.PI, 1 ,0, 0);
+    this.bottomWing.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+    this.scene.translate(3,0,1.2);
+		this.scene.rotate(Math.PI, 0 ,0, 1);
+    this.scene.scale(0.5,0.5,0.5);
+    this.topWing.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+    this.scene.translate(3,0,1.2);
+		this.scene.rotate(Math.PI, 1 ,0, 0);
+    this.scene.scale(0.5,0.5,0.5);
+    this.bottomWing.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+    this.front.display();
+  this.scene.popMatrix();
+
+  this.scene.pushMatrix();
+    this.scene.rotate(Math.PI,1,0,0);
+    this.back.display();
+  this.scene.popMatrix();
+}
