e6272dd38e7142307b7a0ec68a72b09959c69e8a
José Oliveira <jose.j.p.oliveira.96@gmail.com>
2016-11-20 04:19:16 +0000

    no message

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -830,7 +830,29 @@ MySceneGraph.prototype.parserTorus = function(element) {
 //Loading of the patch from dsx
 MySceneGraph.prototype.parserPatch = function(element) {
     console.log("patch");
-    var controlPoints = [];
+    var controlPoints = [	// U = 0
+      [ // V = 0..3;
+         [ -1.5, -1.5, 0.0, 1 ],
+         [ -2.0, -2.0, 2.0, 1 ],
+         [ -2.0,  2.0, 2.0, 1 ],
+         [ -1.5,  1.5, 0.0, 1 ]
+
+      ],
+      // U = 1
+      [ // V = 0..3
+         [ 0, 0, 3.0, 1 ],
+         [ 0, -2.0, 3.0, 1],
+         [ 0,  2.0, 3.0, 1 ],
+         [ 0,  0, 3.0, 1 ]
+      ],
+      // U = 2
+      [ // V = 0..3
+         [ 1.5, -1.5, 0.0, 1 ],
+         [ 2.0, -2.0, 2.0, 1 ],
+         [ 2.0,  2.0, 2.0, 1 ],
+         [ 1.5,  1.5, 0.0, 1 ]
+      ]
+    ];
 
     return new MyPatch(this.scene,
         this.reader.getInteger(element, 'orderU'),
