16589b17710d60f6f36ff26b60b2a98146c39200
MaggGomes <magg.slb@gmail.cm>
2016-12-27 19:21:47 +0000

    fix of ifx

--- reader/trippples/trippples.js
+++ reader/trippples/trippples.js
@@ -9,8 +9,8 @@ function Trippples(scene, mode){
 
   this.client = new Client();
   this.board;
-  this.player1;
-  this.player2;
+  this.player1 = {line: 9, col: 9};
+  this.player2 = {line: 8, col: 1};
 
   this.init();
 }
@@ -25,16 +25,17 @@ Trippples.prototype.init = function (){
 Trippples.prototype.getPrologBoard = function(){
     var game = this;
 
-  this.client.getPrologRequest("board", function(data) {
-      var result = data.target.responseText;
-      game.board = result.split(',');
-  });  
+    this.client.getPrologRequest("board", function(data) {
+      game.board = JSON.parse(data.target.responseText);
+    });  
 };
 
 Trippples.prototype.getPlayerPosition = function(player){
     var game = this;
 
-  this.client.getPrologRequest(player, function(data) {
-    game.player1= data.target.responseText;
-  });  
+    this.client.getPrologRequest(player, function(data) {
+        var result = JSON.parse(data.target.responseText);
+        game.player1['line'] = result[0];
+        game.player1['col'] = result[1];
+    });  
 };
