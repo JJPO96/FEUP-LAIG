87b10d2cb38db2bde4928523ac4ce301c083e518
MaggGomes <magg.slb@gmail.cm>
2016-12-27 18:06:30 +0000

    getboard now working

--- reader/gameParser/trippplesProlog/handler.pl
+++ reader/gameParser/trippplesProlog/handler.pl
@@ -1,10 +1,14 @@
 :- include('trippples.pl').
 
-% testing
-parse_input(ola, 'ok').
-
-
-parse_input(board, X):- board(X).
-
+% Runs application
 parse_input(run, 'ok'):-
 	run.
+
+% Returna the board of the game
+parse_input(board, X):- board(X).
+
+parse_input(1, [Line, Col]):-
+	player(1, Line, Col, _, _).
+
+parse_input(2, [Line, Col]):-
+	player(2, Line, Col, _, _).
--- reader/trippples/trippples.js
+++ reader/trippples/trippples.js
@@ -4,22 +4,18 @@
  * @param mode
  */
 
-
 function Trippples(scene, mode){
   this.scene=scene;
 
-
   this.client = new Client();
-
-  // COLOCAR AQUI CONSTRUTOR
   this.board;
-
-  this.boardProlog = null;
+  this.player1;
+  this.player2;
 
   this.init();
 }
 
-Trippples.prototype.constructor=Trippples;
+Trippples.prototype.constructor = Trippples;
 
 Trippples.prototype.init = function (){
 
@@ -27,29 +23,17 @@ Trippples.prototype.init = function (){
 };
 
 Trippples.prototype.getPrologBoard = function(){
-var game = this;
-
-  this.client.getPrologRequest("board", function(data) {
-    console.log('chegou aqui');
-    game.boardProlog = data.target.responseText;
-
-    //console.log(game.boardProlog);
-
-    console.log('e aqui');
-  });
-
-  if (typeof game.boardProlog === null){
-      console.log("sim");
-  }
-
-  console.log("nao");
-};
-
-Trippples.prototype.getBoard = function(){
     var game = this;
 
-    console.log("Board ");
-    console.log(game.boardProlog);
-    return this.boardProlog;
+  this.client.getPrologRequest("board", function(data) {
+    game.board = data.target.responseText;
+  });  
 };
 
+Trippples.prototype.getPlayerPosition = function(){
+    var game = this;
+
+  this.client.getPrologRequest("board", function(data) {
+    game.board = data.target.responseText;
+  });  
+};
